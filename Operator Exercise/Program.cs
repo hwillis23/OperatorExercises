6940ebfaaff654a98dfb1ba2ca13b997217b5abe
Hannah Willis <hwillis8817@gmail.com>
2022-03-21 12:06:56 -0500

    completed Operator Exercise

---
+++ Operator Exercise/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Operator_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 17;
+            int b = 4;
+
+            int quotient = a / b;
+            int remainder = a % b;
+
+            if (a == 17 && b == 4)
+            {
+                Console.WriteLine($"17/4 is {quotient} remainder {remainder}");
+
+            }
+            Console.WriteLine("What is the radius of a circle?");
+            var radius = double.Parse(Console.ReadLine());
+            double areaOfCircle = Math.PI * radius * radius;
+
+            Console.WriteLine($"The area of a circle with radius of {radius} is {areaOfCircle}"); 
+        }
+    }
+}
+
+
